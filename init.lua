b4eb67f1c3e77a7a3e2e2dd7b1c10521862629c5
xomachine <xomachiner@gmail.com>
2015-12-10 23:47:09 +0300

    Some refactoring and implementation of suggest behaviour

--- init.lua
+++ init.lua
@@ -1,10 +1,6 @@
 print("Hello nim!")
---local buffer = require("buffer")
 local textadept = require("textadept")
 local events = require("events")
---for k, v in pairs(textadept.editing.autocompleters) do 
-  --print(k..":"..tostring(v))
---end
 local nim_executable = "nimsuggest"
 
 local input_pipe = nil
@@ -35,36 +31,53 @@ function nim_start_server(file)
   return in_stream, o_stream
 end
 
+local requests = {
+  sug = function(tokens) 
+      local modulename, stmtname = string.match(tokens.fullname,
+      "([^%.]+)%.(.+)")
+      if modulename == nil then return tokens.fullname end
+      return stmtname 
+    end,
+  con = function(tokens)
+    local inside = string.match(tokens.data,
+    "proc%s+%((.*)%)")
+    local modulename, stmtname = string.match(tokens.fullname,
+    "([^%.]+)%.(.+)")
+    local point_pos = buffer.current_pos - 2 - string.len(stmtname)
+    if point_pos > 0 and buffer.char_at[point_pos] == 46 then
+      local pre_arg = do_request("sug", point_pos - 2)
+      for i, v in ipairs(pre_arg) do
+        print("PreArgDef: "..v.stmtkind.." - "..v.fullname)
+      end
+      inside = string.gsub(inside,"[^,]+,?%s*", "", 1)
+    end
+    inside = string.gsub(inside,"var%s+", "")
+    inside = string.gsub(inside,",", "")
+    inside = string.gsub(inside,":%s+", ":")
+    return inside
+  end,
 
-function nim_complete(name)
+  use = function(tokens) end,
+  def = function(tokens) end,
+}
+
+function do_request(command, pos)
+
   local dirtyname = ""
-  print("Nim autocompleter call")
   if io.type(input_pipe) ~= "file" or io.type(output_pipe) ~= "file" then
     print("Nimsuggest not started! Starting now...")
     input_pipe, output_pipe = nim_start_server(buffer.filename)
   end
-  local prev_char = ''
-  if buffer.current_pos > 0 then
-    prev_char = buffer.char_at[buffer.current_pos-1]
-  end
-  print(prev_char)
-  local position = tostring(buffer.line_from_position(buffer.current_pos)+1)..
-    ":".. tostring(buffer.column[buffer.current_pos])
+  local position = tostring(buffer.line_from_position(pos)+1)..
+    ":".. tostring(buffer.column[pos]+1)
   local filename = buffer.filename
-  local command = ""
-  if prev_char == 40 then
-    command = "con"
-  elseif prev_char == 46 then
-    command = "sug"
-  else
-    command = "def"
-  end
   local request = command.." "..filename..dirtyname..":"..position
   print(request)
   input_pipe:write(request.."\n")
   input_pipe:flush()
   local no_wait = false
   local answer = nil
+  local token_list = {}
   repeat
     repeat
       answer = output_pipe:read()
@@ -75,11 +88,39 @@ function nim_complete(name)
       break
     end
     print("Answer: "..answer)
-    local tokens = {} 
-    for k in string.gmatch(answer, "%S+") do
-      table.insert(tokens, k)
-      --print(tostring(k))
+    local tokens = {}
+    tokens.reqtype, tokens.stmtkind, tokens.fullname, tokens.data, tokens.path,
+    tokens.line, tokens.col, tokens.comment = string.match(answer, 
+    "(%l%l%l)%s+(sk%u%l+)%s+(%S+)%s+(.+)%s+(%S+)%s+(%d+)%s+(%d+)%s+(\".*\")")
+    if tokens.reqtype ~= nil then
+      for k, v in  pairs(tokens) do
+        print (k..": "..v)
+      end
+      table.insert(token_list, tokens)
     end
   until answer == nil
+  return token_list
+end
+
+function nim_complete(name)
+  print("Nim autocompleter call")
+  local  command = "sug"
+  local shift = 0
+  for i = 1, buffer.column[buffer.current_pos] do
+    local c = buffer.char_at[buffer.current_pos - i]
+    if (c >= 32 and c <= 47)or(c >= 58 and c <= 64)then
+      shift = i - 1
+      break
+    end
+  end
+  local suggestions = {}
+  local token_list = do_request(command, buffer.current_pos)
+  for i, v in pairs(token_list) do
+    if requests[v.reqtype] ~= nil then
+      table.insert(suggestions, requests[v.reqtype](v))
+    end
+  end
+  print("Shift = "..shift)
+  return shift, suggestions
 end
 textadept.editing.autocompleters.nim = nim_complete
