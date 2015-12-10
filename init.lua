d0483429572cc66c5588fa0a40c0709af642eb55
xomachine <xomachiner@gmail.com>
2015-12-11 00:02:17 +0300

    Replacing popen to spawn and implementiation of more adequate suggestion matching

--- init.lua
+++ init.lua
@@ -3,32 +3,16 @@ local textadept = require("textadept")
 local events = require("events")
 local nim_executable = "nimsuggest"
 
-local input_pipe = nil
-local output_pipe = nil
+local handler = nil
 function nim_start_server(file)
-  local tmp_fname = os.tmpname()
-  print("created tmp file with name "..tmp_fname)
-  local in_stream = io.popen(--"tail -f "..tmp_fname.." | "..
-    nim_executable.." --stdin --v2 "..file .." >> "..tmp_fname
-    , "w")
-  local o_stream = io.open(tmp_fname, "r")
+  local handle = spawn(nim_executable.." --stdin --v2 "..file)
   local nim_shutdown_server = function()
-    in_stream:write("quit\n\n")
-    in_stream:flush()
-    in_stream:close()
-    o_stream:close()
-    os.remove(tmp_fname)
+    handle:write("quit\n\n")
+    handle:close()
+    handle:wait()
   end
-  if io.type(in_stream) ~= "file" then
-    print("Error! Cannot start nim process")
-  end
-  if io.type(o_stream) ~= "file" then
-    print("Error! Cannot open output stream")
-  end
-  in_stream:setvbuf("no")
-  o_stream:setvbuf("no")
   events.connect(events.QUIT, nim_shutdown_server)
-  return in_stream, o_stream
+  return handle
 end
 
 local requests = {
@@ -64,27 +48,22 @@ local requests = {
 function do_request(command, pos)
 
   local dirtyname = ""
-  if io.type(input_pipe) ~= "file" or io.type(output_pipe) ~= "file" then
+  if handler == nil or handler:status() ~= "running" then
     print("Nimsuggest not started! Starting now...")
-    input_pipe, output_pipe = nim_start_server(buffer.filename)
+    handler = nim_start_server(buffer.filename)
   end
   local position = tostring(buffer.line_from_position(pos)+1)..
     ":".. tostring(buffer.column[pos]+1)
   local filename = buffer.filename
   local request = command.." "..filename..dirtyname..":"..position
   print(request)
-  input_pipe:write(request.."\n")
-  input_pipe:flush()
-  local no_wait = false
-  local answer = nil
+  handler:write(request.."\n")
+  local answer = ""
   local token_list = {}
   repeat
-    repeat
-      answer = output_pipe:read()
-      -- instead of timer, required replacement
-    until no_wait or answer ~= nil
+      answer = handler:read()
     no_wait = true
-    if answer == nil then
+    if answer == "" then
       break
     end
     print("Answer: "..answer)
@@ -114,7 +93,7 @@ function nim_complete(name)
     end
   end
   local suggestions = {}
-  local token_list = do_request(command, buffer.current_pos)
+  local token_list = do_request(command, buffer.current_pos-shift)
   for i, v in pairs(token_list) do
     if requests[v.reqtype] ~= nil then
       table.insert(suggestions, requests[v.reqtype](v))
