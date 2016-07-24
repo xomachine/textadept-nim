6d1d378f7a1ce37ae4e8fee42296296bf9c91746
xomachine <xomachiner@gmail.com>
2016-07-24 21:05:13 +0300

    Little utilite function for type checks

---
+++ utils.lua
@@ -0,0 +1,20 @@
+local _M = {}
+
+function _M.check_type(t, ...)
+  -- Throws an error in caller function when any of supplied arguments
+  -- has a type different from description in first string argument
+  if type(t) ~= "string"
+  then
+    error("Supplied type description if not a string!")
+  end
+  for i, v in pairs({...})
+  do
+    if type(v) ~= t
+    then
+      error("Argument №"..tostring(i).." with value \""..tostring(v)..
+        "\" is not a "..t.."!", 1)
+    end
+  end
+end
+
+return _M
